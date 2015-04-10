a14058fd1e0d1e500c87df98a2d89eb51ac0c15a
Ana Baraldi <avbaraldi@gmail.com>
2015-04-10 16:03:03 -0300

    changes with dplyr

--- plot1.R
+++ plot1.R
@@ -1,10 +1,14 @@
+#libraries
+library(dplyr)
+
 # read and get the data
 unzip("data/exdata-data-household_power_consumption.zip")
 data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE, na.strings = "?")
 
 # cleaning data
 data <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",]
-data$Date <- strptime(paste(data$Date, data$Time, sep="T"), format = "%d/%m/%YT%T")
+data <- mutate(data, Date = paste(Date, Time, sep="T"))
+data$Date <- strptime(data$Date, format = "%d/%m/%YT%T")
 data <- data[-2]
 
 # making the plot
