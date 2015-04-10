3007766aa0caf1187f07022f822697f3cf847c73
Ana Baraldi <avbaraldi@gmail.com>
2015-04-10 15:46:13 -0300

    finished plot 1

---
+++ plot1.R
@@ -0,0 +1,16 @@
+# read and get the data
+unzip("data/exdata-data-household_power_consumption.zip")
+data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE, na.strings = "?")
+
+# cleaning data
+data <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",]
+data$Date <- strptime(paste(data$Date, data$Time, sep="T"), format = "%d/%m/%YT%T")
+data <- data[-2]
+
+# making the plot
+png("plot1.png", bg = "transparent")
+with(data, hist(Global_active_power, 
+                main = "Global Active Power", 
+                col = "red", 
+                xlab = "Global Active Power (kilowatts)"))
+dev.off()
